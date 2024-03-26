f5a4cb0a447150fa3f1f7bbe56ec4037fd32c905
Luis Filipe  Alves Araujo <luisfilipearaujo7@gmail.com>
2024-03-26 17:36:06 +0000

    changed background from left class and added property border-radius to the profile image

--- portfolio-jedi./src/components/Image.js
+++ portfolio-jedi./src/components/Image.js
@@ -1,6 +1,6 @@
 function Image(){
     return(
-        <img src="https://media.licdn.com/dms/image/C5603AQEUOHI54QrVZQ/profile-displayphoto-shrink_200_200/0/1654181903364?e=1717027200&v=beta&t=G5oleQ9ElJJfacpb8WoNRBdShHXWqWQETJib10lddhU" alt="profilePic"></img>
+        <img src="https://media.licdn.com/dms/image/C5603AQEUOHI54QrVZQ/profile-displayphoto-shrink_200_200/0/1654181903364?e=1717027200&v=beta&t=G5oleQ9ElJJfacpb8WoNRBdShHXWqWQETJib10lddhU" alt="profilePic" className="image"></img>
     )    
 }
 
